67c9caba1a2a7c61fc7572a5a78d8fb71651d40e
Jim9025 <jim0988845382@gmail.com>
2025-12-03 14:05:20 +0800

    Update Sample5_1.java

--- Sample5_1.java
+++ Sample5_1.java
@@ -1,8 +1,14 @@
 class Sample5_1 {
     public static void main(String[] args) {
 
-        Car car1;
-        car1 = new Car();
-        car1.show();
+        int[] test;
+        test = new int[5];
+
+        System.out.println("將值指定給test[10]");
+
+        test[10] = 80;    // 超出範圍，會發生例外
+
+        System.out.println("將80指定給test[10]");
+        System.out.println("順利地執行完畢");
     }
 }
