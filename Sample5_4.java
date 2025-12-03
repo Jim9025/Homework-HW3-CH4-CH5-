e5fafd7e028a9840028ffd374c2c71cc999512d1
Jim9025 <jim0988845382@gmail.com>
2025-12-03 14:00:20 +0800

    Create Sample5_4.java

---
+++ Sample5_4.java
@@ -0,0 +1,11 @@
+package pb;
+
+import pc.Car;   // 匯入套件 pc 的 Car 類別
+
+class Sample5_4 {
+    public static void main(String[] args) {
+
+        Car car1 = new Car();   // 匯入後可直接使用類別名稱
+        car1.show();
+    }
+}
