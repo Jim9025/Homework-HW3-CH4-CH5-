fbc0ab18b8cade3f8074dc9a61a7831d574b8ddb
Jim9025 <jim0988845382@gmail.com>
2025-12-03 14:21:00 +0800

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,21 +1,33 @@
-class Car {
-
+class Car
+{
     private int num;
     private double gas;
 
-    public Car() {
+    public Car()
+    {
         num = 0;
         gas = 0.0;
-        System.out.println("已生產了汽車");
+        System.out.println("已產生了汽車");
     }
 
-    public void setCar(int n, double g) {
-        num = n;
-        gas = g;
-        System.out.println("將車號設定為" + num + "汽油量設定為" + gas);
+    // 宣告此方法可能會拋出 CarException
+    public void setCar(int n, double g) throws CarException
+    {
+        if (g < 0)
+        {
+            CarException e = new CarException();
+            throw e;   // g 小於零 → 拋出例外
+        }
+        else
+        {
+            num = n;
+            gas = g;
+            System.out.println("將車號設為" + num + "，汽油量設為" + gas);
+        }
     }
 
-    public void show() {
+    public void show()
+    {
         System.out.println("車號是" + num);
         System.out.println("汽油量是" + gas);
     }
