9d98b87ffe101cd1e60ac8ec2bed91e1d8212de3
Jim9025 <jim0988845382@gmail.com>
2025-12-03 13:27:44 +0800

    Create Sample4_11.java

---
+++ Sample4_11.java
@@ -0,0 +1,18 @@
+public class Sample4_11 {
+    public static void main(String[] args) {
+
+        Vehicle[] vc;
+        vc = new Vehicle[2];
+
+        vc[0] = new Car(1234, 20.5);   // 第 1 個物件是 Car 類別
+        vc[1] = new Plane(232);        // 第 2 個物件是 Plane 類別
+
+        for (int i = 0; i < vc.length; i++) {
+            if (vc[i] instanceof Car) {
+                System.out.println("第" + (i + 1) + "個物件是Car類別");
+            } else {
+                System.out.println("第" + (i + 1) + "個物件不是Car類別");
+            }
+        }
+    }
+}
